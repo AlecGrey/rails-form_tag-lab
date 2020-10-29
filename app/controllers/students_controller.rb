4f22dd095d22456750d97ce4cdfc6a6f3ff6a052
AlecGrey <alecgrey.coding@gmail.com>
2020-10-28 19:58:34 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,22 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    # Student.create(params[:student])
+    @first_name = params[:student][:first_name]
+    @last_name = params[:student][:last_name]
+    # byebug
+    render new_student_path
+  end
+
+  private
+
+  def sanitize
+    params[:student]
+  end
+
+
 end
